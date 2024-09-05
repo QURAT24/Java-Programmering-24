84bde57cba2f23ebb4ea8a50168527f19e4ba03c
Quratulain Qureshi <quratulain.qureshi@iths.se>
2024-09-05 13:02:15 +0200

    Test Push

--- codealongs/HelloWorld/finished/HelloWorld.java
+++ codealongs/HelloWorld/finished/HelloWorld.java
@@ -2,7 +2,7 @@ package codealongs.HelloWorld.finished;
 
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hey world, how are you?");
+        System.out.println("Hello world");
 
     }
 }
